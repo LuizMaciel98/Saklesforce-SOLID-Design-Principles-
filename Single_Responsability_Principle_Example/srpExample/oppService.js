793e75d93b49404e72dab71900c49851fc932b0f
Luiz Maciel Segura dos Santos <luiz.maciel98@hotmail.com>
2022-06-27 00:34:56 -0300

    Single Responsibility Principle

---
+++ Single_Responsability_Principle_Example/srpExample/oppService.js
@@ -0,0 +1,16 @@
+import {oppServiceCTO} from "./oppServiceCTO";
+import {oppServiceManager} from "./oppServiceManager";
+import {oppServiceFactory} from "./oppServiceFactory";
+
+export class oppService {
+    calculateOpps(userType) {
+        let classType;
+        if (userType === 'CTO') {
+            classType = new oppServiceCTO();
+        } else if (userType === 'Manager') {
+            classType = new oppServiceManager();
+        }
+        const oppServiceFactory1 = new oppServiceFactory(classType);
+        oppServiceFactory1.calculateOpps();
+    }
+}
